c7c369c8947899ab509bf2b8de78a366d8492479
Luku <luku.3659@gmail.com>
2022-06-30 10:43:34 +0900

    update dependency version.

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,32 +1,33 @@
+use config::{Config, Environment};
 use once_cell::sync::Lazy;
 use serde::Deserialize;
 use smart_default::SmartDefault;
 
 #[derive(Debug, Deserialize, SmartDefault)]
-pub struct Config {
+pub struct AppConfig {
     #[default = "./dm.db"]
     pub database_url: String,
 }
 
-impl Config {
+impl AppConfig {
     pub fn from_env() -> Self {
-        let mut cfg = config::Config::new();
-        let result = cfg.merge(config::Environment::with_prefix("DM"));
-        if result.is_ok() {
-            let result = cfg.try_into();
-            return match result {
-                Ok(config) => {
-                    debug!("{:?}", config);
-                    config
-                },
-                Err(err) => {
-                    debug!("{:?}", err);
-                    Self::default()
-                },
-            };
+        let cfg = Config::builder()
+            .add_source(Environment::with_prefix("DM"))
+            .build();
+
+        match cfg {
+            Ok(config) => {
+                // TODO avoid using unwrap
+                let app_config: AppConfig = config.try_deserialize().unwrap();
+                debug!("{:?}", app_config);
+                app_config
+            }
+            Err(e) => {
+                debug!("{:?}", e);
+                Self::default()
+            }
         }
-        Self::default()
     }
 }
 
-pub static CONFIG: Lazy<Config> = Lazy::new(Config::from_env);
+pub static CONFIG: Lazy<AppConfig> = Lazy::new(AppConfig::from_env);
